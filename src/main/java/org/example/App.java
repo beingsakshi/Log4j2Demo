84efcaac36a1361e973267f0c6154823e350272f
beingsakshi <beingsakshiagarwal@gmail.com>
2022-08-30 14:24:47 +0530

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        logger.trace("Checking log");
+        logger.debug("Logging");
+        logger.warn("logging");
+        logger.error("Error msg");
+        logger.info("Greeted the user");
+        logger.fatal("Ftal msg");
+    }
+}
